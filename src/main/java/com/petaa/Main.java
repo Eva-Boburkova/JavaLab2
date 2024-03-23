13dd47dce6fbeebe8d51c6b62b237c32a5c392a6
Eva-Boburkova <eva.boburkova33@mail.ru>
2024-03-23 14:47:14 +0300

    Main update

--- src/main/java/com/petaa/Main.java
+++ src/main/java/com/petaa/Main.java
@@ -1,7 +1,26 @@
 package com.petaa;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите арифметическое выражение");
+        String exp = sc.nextLine();
 
+        Optional<Solution> optionalSolution = Optional.empty();
+
+        try {
+            optionalSolution = Optional.of(new Solution(exp));
+        }
+        catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+
+        if(optionalSolution.isPresent()){
+            Solution sln = optionalSolution.get();
+            System.out.printf("%s = %f", exp, sln.solve());
+        }
     }
 }
