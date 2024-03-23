572bef11deded4d1abf7399a63b1652eae22c4b1
Eva-Boburkova <eva.boburkova33@mail.ru>
2024-03-23 15:41:56 +0300

    to complete

--- src/main/java/com/petaa/Solution.java
+++ src/main/java/com/petaa/Solution.java
@@ -1,11 +1,14 @@
 package com.petaa;
 
+import java.util.Stack;
+
 /**
  * Утилитарный класс для решения задачи
  * @author Ева Б.
  */
 public class Solution {
     private String exp;
+    private final String OPS = "+-/*";
 
     /**
      * Конструктор с проверкой на корректность
@@ -18,9 +21,15 @@ public class Solution {
 
     /**
      * Проверка на корректность
+     * @throws RuntimeException при некорректном выражении
      */
     private void checkExp(){
         //TODO написать проверки: 1. несоответствие скобок 2.два знака подряд 3.пробел между числами
+        boolean isValid = true;
+        //..
+
+        if(!isValid)
+            throw new RuntimeException("Expression is invalid");
     }
 
     /**
@@ -29,7 +38,37 @@ public class Solution {
      */
     public float solve(){
         float result = 0;
-        //TODO написать решение. строка -> стек -> ычисление
+        Stack<Character> cup = new Stack<>();
+        boolean isInBraces = false;
+        boolean isSign = false;
+
+        for(int i = 0; i < exp.length(); i++)
+        {
+            cup.add(exp.charAt(i));
+            if(cup.peek() == '('){
+                isInBraces = true;
+            }
+            if(cup.peek() == ')' && isInBraces) {
+                cup.pop(); //убрали только что сунутую )
+                int a = 0;
+                int b = 0;
+                int n = 0; //разряд цифры
+                while(cup.peek() != '('){
+                    char c = cup.pop();
+                    if(OPS.indexOf(c) != -1){
+                        isSign = true;
+                        n = 0;
+                        b = a;
+                        a = 0;
+                        continue;
+                    }
+
+                    a += (int) ((int) c * Math.pow(10, n));
+                    n++;
+                }
+            }
+        }
+
         return result;
     }
 }
