98b2e5f0dcc8495212fb5379ff569012103c321b
Артем <tema.petrosyan@gmail.com>
2024-03-23 14:09:42 +0300

    Solution class definition

--- src/main/java/com/petaa/Solution.java
+++ src/main/java/com/petaa/Solution.java
@@ -1,5 +1,35 @@
 package com.petaa;
 
+/**
+ * Утилитарный класс для решения задачи
+ * @author Ева Б.
+ */
 public class Solution {
+    private String exp;
 
+    /**
+     * Конструктор с проверкой на корректность
+     * @param exp выражение в текстовой форме
+     */
+    public Solution(String exp){
+        checkExp();
+        this.exp = exp;
+    }
+
+    /**
+     * Проверка на корректность
+     */
+    private void checkExp(){
+        //TODO написать проверки: 1. несоответствие скобок 2.два знака подряд 3.пробел между числами
+    }
+
+    /**
+     * Функция для решения заданного выражения
+     * @return Числовое значение выражения
+     */
+    public float solve(){
+        float result = 0;
+        //TODO написать решение. строка -> стек -> ычисление
+        return result;
+    }
 }
